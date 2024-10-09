ee7f1d8df6d0684e9033dda390e33245da7d468c
NEXIZ0 <146828807+NEXIZ0@users.noreply.github.com>
2024-10-09 16:17:44 +0330

    Update super_fuzz.sh

--- super_fuzz.sh
+++ super_fuzz.sh
@@ -12,8 +12,8 @@ super_fuzz() {
   # Read and print each line of the file
   while IFS= read -r line || [[ -n "$line" ]]; do
     # Ensure line is not empty and the file from the list exists before running ffuf
-    if [[ -n "$line" && -f "Desktop/world-list/$line" ]]; then
-      ffuf -w "Desktop/world-list/$line" -u "$url/FUZZ" -mc all -c -H "user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36" $filter
+    if [[ -n "$line" && -f "~/Desktop/world-list/$line" ]]; then
+      ffuf -w "~/Desktop/world-list/$line" -u "$url/FUZZ" -mc all -c -H "user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36" $filter
       echo "#############--------------------------------########"
     else
       echo "Warning: wordlist file not found for '$line'"
