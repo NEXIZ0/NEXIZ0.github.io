881a5adca27f46f5f0524d12fe873e52262c374c
NEXIZ0 <146828807+NEXIZ0@users.noreply.github.com>
2024-10-09 15:40:32 +0330

    Update super_fuzz.sh

--- super_fuzz.sh
+++ super_fuzz.sh
@@ -1,12 +1,21 @@
-super_fuzz() {
+print_lines() {
   url="$1"
   filter="$2"
   local file="/root/test.txt"
   
+  # Check if the file exists
+  if [[ ! -f "$file" ]]; then
+    echo "File not found: $file"
+    return 1
+  fi
+
   # Read and print each line of the file
-  while IFS= read -r line; do
-    ffuf -w "Desktop/world-list/$line" -u "$url/FUZZ" -mc all -c -H "user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36" "$filter"
-    
-    echo "################################################################"
+  while IFS= read -r line || [[ -n "$line" ]]; do
+    # Ensure line is not empty before running ffuf
+    if [[ -n "$line" ]]; then
+      ffuf -w "Desktop/world-list/$line" -u "$url/FUZZ" -mc all -c -H "user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36" $filter
+      
+      echo "#############--------------------------------########"
+    fi
   done < "$file"
 }
